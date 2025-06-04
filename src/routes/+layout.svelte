cf287196deed8115c80195ba5280d4ec1fd73c18
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2025-06-04 15:43:31 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,11 @@
 	import authStore from '$lib/stores/authStore';
 	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 </script>
 
 <div class="min-h-screen bg-slate-50">
-	{#if $authStore.loading && $page.url.pathname !== '/login'}
+	{#if $authStore.loading && $page.url.pathname !== base + '/login'}
 		<div class="flex items-center justify-center min-h-[calc(100vh-4rem)]">
 			<div class="text-center">
 				<LoadingSpinner size="w-12 h-12" />
