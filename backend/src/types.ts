702606974b9531f85dc9b87faea4c603d6e27a27
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2025-07-11 15:18:32 +0200

    WIP

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,11 +1,18 @@
 import type { Context } from "hono";
 import { GoPayClient } from "./goPay/client";
+import { GoPayClientMock } from "./goPay/client.mock";
 
 export type AppContext = Context<{ Bindings: Env }>;
 
 export const createGoPayClient = (context: AppContext): GoPayClient => {
-  return new GoPayClient(
-    context.env.GOPAY_API_URL,
-    context.req.header("Authorization").replace("Bearer", "").trim()
-  );
+  const apiUrl = context.env.GOPAY_API_URL;
+  const token = context.req
+    .header("Authorization")
+    .replace("Bearer", "")
+    .trim();
+
+  if (context.env.USE_LOCAL_MOCK_CLIENTS === true) {
+    return new GoPayClientMock(apiUrl, token);
+  }
+  return new GoPayClient(apiUrl, token);
 };
