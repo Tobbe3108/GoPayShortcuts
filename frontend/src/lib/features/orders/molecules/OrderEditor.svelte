6993281581c59f7b23cd1d4ade0fba8229e12a2e
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2025-09-23 08:21:44 +0200

    feat(order-editor): compute and use derived total and adjust table layout

--- frontend/src/lib/features/orders/molecules/OrderEditor.svelte
+++ frontend/src/lib/features/orders/molecules/OrderEditor.svelte
@@ -46,10 +46,12 @@
 		})
 	);
 
+	const totalPrice = $derived(editableOrderlines.reduce((sum, l) => sum + l.price * l.quantity, 0));
+
 	function handleQuantityChange(idx: number, newValue: number) {
 		editableOrderlines[idx] = { ...editableOrderlines[idx], quantity: newValue };
 		order.orderlines = editableOrderlines;
-		order.totalPrice = editableOrderlines.reduce((sum, l) => sum + l.price * l.quantity, 0);
+		order.totalPrice = totalPrice;
 	}
 
 	function formatPrice(amount: number) {
@@ -62,20 +64,22 @@
 		<tbody>
 			{#each items as item, idx}
 				<tr>
-					<td class="py-1">{item.name}</td>
-					<td class="py-1 text-center w-1/4">
-						{#if editMode}
-							<Quantity
-								value={item.quantity}
-								min={0}
-								max={99}
-								onChange={(v) => handleQuantityChange(idx, v)}
-							/>
-						{:else}
-							{item.quantity}
-						{/if}
+					<td class="py-1 w-4/6">{item.name}</td>
+					<td class="py-1 w-1/6">
+						<div class="flex justify-center">
+							{#if editMode}
+								<Quantity
+									value={item.quantity}
+									min={0}
+									max={99}
+									onChange={(v) => handleQuantityChange(idx, v)}
+								/>
+							{:else}
+								{item.quantity}
+							{/if}
+						</div>
 					</td>
-					<td class="py-1 text-right">{formatPrice(item.price * item.quantity)}</td>
+					<td class="py-1 text-right w-1/6">{formatPrice(item.price * item.quantity)}</td>
 				</tr>
 			{/each}
 		</tbody>
@@ -83,7 +87,7 @@
 			<tfoot>
 				<tr class="font-semibold border-t border-slate-200">
 					<td class="py-1" colspan="2">Total</td>
-					<td class="py-1 text-right w-1/4">{formatPrice(order.totalPrice ?? 0)}</td>
+					<td class="py-1 text-right w-1/4">{formatPrice(totalPrice)}</td>
 				</tr>
 			</tfoot>
 		{/if}
