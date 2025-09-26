2c3629182467fdcfee45d8d889e7f1215b277e12
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2025-09-26 13:42:20 +0200

    feat(week): add WeekGridTemplate component scaffolding

---
+++ frontend/src/lib/components/templates/WeekGridTemplate.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import DayHeader from '$lib/components/molecules/DayHeader.svelte';
+	import OrderCard from '$lib/features/orders/organisms/OrderCard.svelte';
+	import AddLocationCard from '$lib/features/locations/molecules/AddLocationCard.svelte';
+	import WeekNavigator from '$lib/components/molecules/WeekNavigator.svelte';
+	import { startOfWeek, endOfWeek, getWeek, addDays } from 'date-fns';
+	import { ordersService } from '$lib/features/orders/ordersService';
+	import { getIsoDate } from '$lib/core/utils/dateUtils';
+	import type { Order } from '$lib/features/orders/models/order';
+
+	type WeekGridProps = {
+		date: Date;
+	};
+
+	let { date }: WeekGridProps = $props();
+
+	let weekNumber = $state(getWeek(date));
+	let weekStart = $derived(startOfWeek(weekNumber));
+	let weekEnd = $derived(endOfWeek(weekNumber));
+	let weekDates = $derived(Array.from({ length: 5 }, (_, i) => addDays(weekStart, i)));
+
+	let orders: Order[] = $state([]);
+	$effect(() => async () => (orders = await ordersService.listOrders(weekStart, weekEnd)));
+
+	function ordersByDay(date: Date) {
+		return orders.filter((o) => o.date === getIsoDate(date));
+	}
+</script>
+
+<div>
+	<WeekNavigator
+		{date}
+		onWeekChange={(newWeek) => {
+			weekNumber = newWeek;
+		}}
+	/>
+
+	<!-- <div class="grid grid-cols-5 gap-4">
+		{#each weekDates as date}
+			<div class="flex flex-col space-y-4">
+				<DayHeader {date} />
+				{#each ordersByDay(date) as order}
+					<OrderCard {order} onOrderChange={() => {}} />
+				{/each}
+				<AddLocationCard
+					newOrder={(order) => {}}
+					locationsWithOrders={ordersByDay(date).map((o) => o.kitchenId)}
+				/>
+			</div>
+		{/each}
+	</div> -->
+</div>
