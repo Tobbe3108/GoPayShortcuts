7e8fb91708a622f7195cdaa2abba25f28d939c14
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2026-01-06 10:47:51 +0100

    Revert "feat: implement simple i18n localization system for Danish and English"
    This reverts commit a067a13d0cb1cf6e5d12e7227f2c1be611509eb2.

--- frontend/src/lib/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * i18n (Internationalization) module for GoPayShortcuts
- * Provides localization support for Danish (da) and English (en)
- */
-
-export { locale, translations, t, detectBrowserLanguage, type Locale } from './store';
